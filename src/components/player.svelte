d9a1434a499d452aa1f8dfc101bbd75699644d79
Elijah Mooring <ejsisgreat@gmail.com>
2020-08-05 06:08:45 -0500

    Editing and deleting players

--- src/components/player.svelte
+++ src/components/player.svelte
@@ -1,20 +1,70 @@
 <script>
+	import { players } from '../store';
+	import UI from '../ui';
+	import PlayerInfo from './player-info.svelte';
+	import { tick } from 'svelte';
+
 	export let name;
 	export let age;
 	export let weight;
+	export let id;
 
-	let hovering = false;
+	let editing = false;
+	let editName = name;
+	let editAge = age;
+	let editWeight = weight;
+
+	function edit() {
+		setTimeout(() => (editing = true), 50);
+	}
+
+	function del() {
+		$players = $players.filter(player => player.id !== id);
+	}
+
+	function cancel() {
+		editing = false;
+		editName = name;
+		editAge = age;
+		editWeight = weight;
+	}
+
+	function done() {
+		editing = false;
+		$players = $players.map(player => {
+			if (player.id === id) return { name: editName, age: editAge, weight: editWeight, id };
+			else return player;
+		});
+	}
 </script>
 
-<div class="over" on:mouseover={() => (hovering = true)} on:mouseout={() => (hovering = false)}>
+<div class="over">
 	{name} - {age} ({weight})
-	{#if hovering}
-		<div class="popover">
-			<div class="half edit">E</div>
-			<div class="half delete">D</div>
-		</div>
-	{/if}
+	<div class="popover">
+		<div class="half edit" on:click={edit}>E</div>
+		<div class="half delete" on:click={del}>D</div>
+	</div>
 </div>
+
+{#if editing}
+	<UI.Modal onCancel={cancel}>
+		<div class="container">
+			<h3>Add Player</h3>
+
+			<PlayerInfo bind:name={editName} bind:weight={editWeight} bind:age={editAge} />
+
+			<div class="pad20" />
+
+			<div style="text-align: right">
+				<UI.Button on:click={cancel}>Cancel</UI.Button>
+				&nbsp;&nbsp;
+				<UI.Button on:click={done}>Save</UI.Button>
+			</div>
+
+			<div class="pad20" />
+		</div>
+	</UI.Modal>
+{/if}
 
 <style>
 	.over {
@@ -26,6 +76,7 @@
 	}
 
 	.popover {
+		display: none;
 		position: absolute;
 		top: -10px;
 		right: -25px;
@@ -33,7 +84,13 @@
 		width: 50px;
 		border-radius: 4px;
 		overflow: hidden;
+		z-index: 30;
 	}
+
+	.over:hover .popover {
+		display: block;
+	}
+
 	.half {
 		float: left;
 		width: 50%;
